b0adc5d64d9a4039d1ebd87a07e75246fd5900c4
Denis <ddenis@mail.ru>
2024-04-19 16:20:46 +0300

    plot

---
+++ src/main/java/dev/kefir213/minigamesstudio/MiniGamesStudioApplication.java
@@ -0,0 +1,13 @@
+package dev.kefir213.minigamesstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniGamesStudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniGamesStudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kefir213/minigamesstudio/MiniGamesStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kefir213.minigamesstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGamesStudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
